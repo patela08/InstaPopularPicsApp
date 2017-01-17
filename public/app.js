98bcf660dc4d40e26f975e17441c051dc453b08f
patela08 <patelakshar.patel98@gmail.com>
2017-01-17 18:44:51 -0500

    needed to fix authentication

--- public/app.js
+++ public/app.js
@@ -1,12 +1,12 @@
 (function(){
-    var app = angular.module('photoGal',['ngRoute']);
+    var app = angular.module('photoGal',['ngRoute','ngResource']);
 
     app.config(['$routeProvider',rProvide]);
 
     function rProvide($routeProvider){
         $routeProvider
             .when('/Gallery',{
-                templateUrl: 'views.gallery.view.html',
+                templateUrl: 'view/gallery.view.html',
                 controller: 'GalleryController',
                 controllerAs: 'gCtrl'
             })
--- public/controller/gallery.controller.js
+++ public/controller/gallery.controller.js
@@ -2,9 +2,55 @@
  * Created by abpatel on 1/17/2017.
  */
 (function () {
+   'use strict'
    angular.module('photoGal')
-       .controller('galController',galleryFun);
-   function galleryFun(){
+       .controller('GalleryController',['$sce','$http',GalleryController]);
+   // function galleryFun($sce,$http){
+   //    var vm = this;
+   //    vm.images = [];
+   //    vm.url = $sce.trustAsResourceUrl("https://api.instagram.com/v1/users/self/media/recent?access_token=ACCESS_TOKEN&count=35")
+   //    console.log(vm);
+   //    // photoFact.fetchPopular(function(data){
+   //    //    console.log(data);
+   //    //    vm.images = data;
+   //    // })
+   //     $http.jsonp(vm.url)
+   //         .then(function (response) {
+   //
+   //             vm.images=response.data.data;
+   //             console.log(response);
+   //             angular.forEach(vm.images,function (value) {
+   //                 imgArray.push(value.images.standard_resolution);
+   //             });
+   //             // gc.openLightboxModal = function (index) {
+   //             //     Lightbox.openModal(imgArray,index);
+   //             // }
+   //
+   //         },(function (error) {
+   //             console.log(error);
+   //         }));
+   // }
 
-   }
+    function GalleryController($sce,$http) {
+        var gc = this;
+        gc.Url = $sce.trustAsResourceUrl('https://api.instagram.com/v1/users/self/media/recent/?access_token=YOUR_ACCESS_TOKEN&count=35');
+
+        gc.images = [];
+        var imgArray = [];
+
+        $http.jsonp(gc.Url)
+            .then(function (response) {
+                console.log(response)
+                gc.images=response.data.data;
+                angular.forEach(gc.images,function (value) {
+                    imgArray.push(value.images.standard_resolution);
+                });
+                // gc.openLightboxModal = function (index) {
+                //     Lightbox.openModal(imgArray,index);
+                // }
+
+            },(function (error) {
+                console.log(error);
+            }));
+    }
 })()
